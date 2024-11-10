40c8a21d8515bb49a9f27f6f815a322bf4755267
CASPARS-SURFACE\c_van <dandykong@gmail.com>
2024-11-10 23:39:33 +0100

    Full flow in CommandLineRunner

--- src/main/java/com/dandykong/klinkendetaal/KlinkendeTaalApplication.java
+++ src/main/java/com/dandykong/klinkendetaal/KlinkendeTaalApplication.java
@@ -1,16 +1,21 @@
 package com.dandykong.klinkendetaal;
 
+import com.dandykong.klinkendetaal.model.dictionary.Dictionary;
 import com.dandykong.klinkendetaal.model.tokenizer.Tokenizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.File;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 
 @SpringBootApplication
 public class KlinkendeTaalApplication {
@@ -21,12 +26,24 @@ public class KlinkendeTaalApplication {
 		return new Tokenizer();
 	}
 
-	// run with "src/test/resources/Formeel.txt" as command line argument
+	@Bean
+	public Dictionary dictionary(ApplicationArguments arguments) {
+		Dictionary dictionary = new Dictionary();
+		String dictionaryFilePath = arguments.getSourceArgs()[0];
+		File dictionaryFile = new File(dictionaryFilePath);
+		dictionary.read(dictionaryFile);
+		return dictionary;
+	}
+
+	// run with
+	// 	- "src/test/resources/Dictionary.json"
+	// 	- "src/test/resources/Formeel.txt"
+	// 	as command line arguments
 	@Bean
 	public CommandLineRunner run(Tokenizer tokenizer) {
 		return args -> {
 			LOG.info("Starting application");
-			String input = Files.readString(Path.of(args[0]), Charset.forName("utf-8"));
+			String input = Files.readString(Path.of(args[1]), StandardCharsets.UTF_8);
 			tokenizer.tokenize(input).forEach(System.out::println);
 			LOG.info("Finishing application");
 		};
